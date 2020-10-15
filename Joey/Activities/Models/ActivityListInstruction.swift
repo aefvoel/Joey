8564413bb2e2cd1742a871adc4a53481af34994a
Toriq Wahid Syaefullah <toriqwahidsyaef@gmail.com>
2020-10-15 12:47:15 +0700

    added activities intruction

---
+++ Joey/Activities/Models/ActivityListInstruction.swift
@@ -0,0 +1,30 @@
+//
+//  ActivityListInstruction.swift
+//  Joey
+//
+//  Created by Setiawan Joddy on 14/10/20.
+//
+import Foundation
+
+struct ActivitiesInstruction {
+    var activityTitle: String
+    var instructionContent: String
+    var activityHelpsContent: String
+    var basedOnResearch: String
+}
+
+var activitiesInstructionArray =
+    [//Thought Records
+        ActivitiesInstruction(activityTitle: "Thought Records", instructionContent: "Ini Instruksi Bakal EE", activityHelpsContent: "Pokoknya Ini akan membantumu!", basedOnResearch: "Aduh, Bagus Pokoknya"),
+        //Mirror Technique
+        ActivitiesInstruction(activityTitle: "Mirror Technique", instructionContent: "Ayodong Cerita", activityHelpsContent: "Dijamin 100% Makin PD", basedOnResearch: "Terpercaya"),
+        //Jacobson's Technique
+        ActivitiesInstruction(activityTitle: "Jacobson's Technique", instructionContent: "Tabok aja bor", activityHelpsContent: "Bikin emosimu hilang", basedOnResearch: "Semangatt"),
+        //Practice Gratitude
+        ActivitiesInstruction(activityTitle: "Practice Gratitude", instructionContent: "Napas aja sih", activityHelpsContent: "Dijamin panjang umur", basedOnResearch: "Skuyyy"),
+        //Smile
+        ActivitiesInstruction(activityTitle: "Smile", instructionContent: "Pecahkan gelembung", activityHelpsContent: "Yaudah si tinggal pecahin", basedOnResearch: "Bikin Candu"),
+        //Breath
+        ActivitiesInstruction(activityTitle: "Breath", instructionContent: "Ayodong Cerita", activityHelpsContent: "Dijamin 100% Makin PD", basedOnResearch: "Terpercaya"),
+        //Express Emotion
+        ActivitiesInstruction(activityTitle: "Express Emotion", instructionContent: "Ayodong Cerita", activityHelpsContent: "Dijamin 100% Makin PD", basedOnResearch: "Terpercaya")]
