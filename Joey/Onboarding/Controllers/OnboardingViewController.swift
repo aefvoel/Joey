0f16e7ce1bd843dc55fda8f6ad20bd68bb86ee6e
Setiawan Joddy <37212064+setiawanjoddy@users.noreply.github.com>
2020-10-19 06:26:13 +0700

    Onboard assets added, "Welcome" and "Insert Name" added.

--- Joey/Onboarding/Controllers/OnboardingViewController.swift
+++ Joey/Onboarding/Controllers/OnboardingViewController.swift
@@ -8,14 +8,29 @@
 import UIKit
 
 class OnboardingViewController: UIViewController {
-
+    
+    @IBOutlet weak var buttonOnboarding: UIButton!
+    @IBOutlet weak var labelName: UITextField!
+    @IBOutlet weak var viewLabelBackground: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        self.navigationController?.isNavigationBarHidden = true
         // Do any additional setup after loading the view.
+        buttonOnboarding?.layer.cornerRadius = 14.0
+        labelName?.layer.cornerRadius = 14.0
+        viewLabelBackground?.layer.cornerRadius = 14.0
+        
     }
-    
 
+    @IBAction func onClickButtonEnterName(_ sender: Any) {
+        if labelName.text == "" {
+            //AlertHelper.createAlert(title: "Okay", message: "test", onComplete: <#T##() -> Void#>)
+        }
+        else {
+            UserDefaultsHelper.setData(value: labelName.text, key: .userName)
+        }
+    }
     /*
     // MARK: - Navigation
 
