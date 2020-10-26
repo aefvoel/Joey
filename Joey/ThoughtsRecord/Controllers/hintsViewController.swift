6bfd456815ec8b991475c8e753f55f3d8ab69ce5
Setiawan Joddy <37212064+setiawanjoddy@users.noreply.github.com>
2020-10-26 12:04:12 +0700

    Thought Records Initial Page Added

---
+++ Joey/ThoughtsRecord/Controllers/hintsViewController.swift
@@ -0,0 +1,36 @@
+//
+//  WhyItWorksViewController.swift
+//  Joey
+//
+//  Created by Setiawan Joddy on 26/10/20.
+//
+
+import UIKit
+
+class hintsViewController: UIViewController {
+
+    @IBOutlet weak var textView: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        textView.isEditable = false
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func onClickDismissButton(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
