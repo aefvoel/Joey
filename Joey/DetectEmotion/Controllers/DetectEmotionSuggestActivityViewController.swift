1ffaddeed0b88eb93ac6819e803f3617479eedea
Rahman Fadhil <rhmnfadhil@gmail.com>
2020-10-27 12:55:16 +0700

    Finish detect emotion 🥳

--- Joey/DetectEmotion/Controllers/DetectEmotionSuggestActivityViewController.swift
+++ Joey/DetectEmotion/Controllers/DetectEmotionSuggestActivityViewController.swift
@@ -10,23 +10,41 @@ import UIKit
 class DetectEmotionSuggestActivityViewController: UIViewController {
 
     var data: FollowUp?
+    var instruction: ActivitiesInstruction?
     @IBOutlet weak var navBar: NavigationBar!
+    @IBOutlet weak var suggestionLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         navBar.delegate = self
-        let activity = data?.suggestActivity()
+        if let activity = data?.suggestActivity() {
+            instruction = activity
+            suggestionLabel.text = "This is the activity that you can do to calm yourself, \(activity.title).\nDo you want to start now?"
+        }
     }
     
+    func popTo<T>(_ vc: T.Type) {
+       let targetVC = navigationController?.viewControllers.first{$0 is T}
+       if let targetVC = targetVC {
+          navigationController?.popToViewController(targetVC, animated: true)
+       }
+    }
 
-    /*
+    @IBAction func onStartButtonTapped(_ sender: Any) {
+        performSegue(withIdentifier: "toInstruction", sender: nil)
+    }
+    
+    @IBAction func onNoButtonTapped(_ sender: Any) {
+        popTo(HomeViewController.self)
+    }
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        if let vc = segue.destination as? InstructionViewController {
+            vc.activityInstruction = instruction
+        }
     }
-    */
 
 }
