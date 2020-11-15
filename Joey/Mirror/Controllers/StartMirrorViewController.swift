09ee49c763d0d850dc88f7ee3ea783d871a28057
Rahman Fadhil <rhmnfadhil@gmail.com>
2020-11-15 10:30:52 +0700

    Fix mirror technique

--- Joey/Mirror/Controllers/StartMirrorViewController.swift
+++ Joey/Mirror/Controllers/StartMirrorViewController.swift
@@ -81,8 +81,10 @@ class StartMirrorViewController: UIViewController {
                 if let hint = self.currentHint?.plainText, let words = result?.bestTranscription.formattedString {
                     print("words: \(words)")
                     print("hints: \(hint)")
-                    if words.lowercased().hasSuffix(hint.lowercased()) {
-                        self.randomHint()
+                    for text in hint {
+                        if words.lowercased().hasSuffix(text.lowercased()) {
+                            self.randomHint()
+                        }
                     }
                 }
                 isFinal = (result?.isFinal)!
--- Joey/Mirror/Models/MirrorHint.swift
+++ Joey/Mirror/Models/MirrorHint.swift
@@ -9,84 +9,84 @@ import Foundation
 
 struct MirrorHint {
     let hint: String
-    let plainText: String
+    let plainText: [String]
     
     static let hints = [
         MirrorHint(
             hint: "I believe that I am worthy of love",
-            plainText: "I believe that I am worthy of love"
+            plainText: ["I believe that I am worthy of love", "I believe that I'm worthy of love"]
         ),
         MirrorHint(
             hint: "I'm different in my own beautiful way",
-            plainText: "I am different in my own beautiful way"
+            plainText: ["I am different in my own beautiful way", "I'm different in my own beautiful way"]
         ),
         MirrorHint(
             hint: "I forgive everyone that has ever hurt me",
-            plainText: "I forgive everyone that has ever hurt me"
+            plainText: ["I forgive everyone that has ever hurt me"]
         ),
         MirrorHint(
             hint: "I have the power to create the life that I desire",
-            plainText: "I have the power to create the life that I desire"
+            plainText: ["I have the power to create the life that I desire"]
         ),
         MirrorHint(
             hint: "I'm allowed to say \"NO\" to others and \"YES\" to myself",
-            plainText: "I am allowed to say no to others and yes to myself"
+            plainText: ["I am allowed to say no to others and yes to myself", "I'm allowed to say no to others and yes to myself"]
         ),
         MirrorHint(
             hint: "I forgive myself for all the mistakes that I have made",
-            plainText: "I forgive myself for all the mistakes that I have made"
+            plainText: ["I forgive myself for all the mistakes that I have made"]
         ),
         MirrorHint(
             hint: "I'm talented and intelligent",
-            plainText: "I am talented and intelligent"
+            plainText: ["I am talented and intelligent", "I'm talented and intelligent"]
         ),
         MirrorHint(
             hint: "I'm thankful for all that I have",
-            plainText: "I am thankful for all that I have"
+            plainText: ["I am thankful for all that I have", "I'm thankful for all that I have"]
         ),
         MirrorHint(
             hint: "I am brave. I am bold. I am strong",
-            plainText: "I am brave I am bold I am strong"
+            plainText: ["I am brave I am bold I am strong", "I'm brave I'm bold I'm strong"]
         ),
         MirrorHint(
             hint: "I'm in control of my thoughts, feelings, and choices",
-            plainText: "I am in control of my thoughts feelings and choices"
+            plainText: ["I am in control of my thoughts feelings and choices", "I'm in control of my thoughts feelings and choices"]
         ),
         MirrorHint(
             hint: "I will not compare myself to strangers on the internet",
-            plainText: "I will not compare myself to strangers on the internet"
+            plainText: ["I will not compare myself to strangers on the internet", "I will not compare myself to strangers on the internet"]
         ),
         MirrorHint(
             hint: "I deserve the best and I accept the best",
-            plainText: "I deserve the best and I accept the best"
+            plainText: ["I deserve the best and I accept the best"]
         ),
         MirrorHint(
             hint: "I'm blessed with a wonderful family and amazing friends",
-            plainText: "I am blessed with a wonderful family and amazing friends"
+            plainText: ["I am blessed with a wonderful family and amazing friends", "I'm blessed with a wonderful family and amazing friends"]
         ),
         MirrorHint(
             hint: "I let go of all that no longer serves me",
-            plainText: "I let go of all that no longer serves me"
+            plainText: ["I let go of all that no longer serves me"]
         ),
         MirrorHint(
             hint: "My needs and wants are important",
-            plainText: "my needs and wants are important"
+            plainText: ["my needs and wants are important"]
         ),
         MirrorHint(
             hint: "My contributions to the world are valuable",
-            plainText: "my contributions to the world are valuable"
+            plainText: ["my contributions to the world are valuable"]
         ),
         MirrorHint(
             hint: "I'm open to new adventures in my life",
-            plainText: "I am open to new adventures in my life"
+            plainText: ["I am open to new adventures in my life", "I'm open to new adventures in my life"]
         ),
         MirrorHint(
             hint: "I'm smart enough to make my own decisions",
-            plainText: "I am smart enough to make my own decisions"
+            plainText: ["I am smart enough to make my own decisions", "I'm smart enough to make my own decisions"]
         ),
         MirrorHint(
             hint: "I know exactly what to do to achieve success",
-            plainText: "I know exactly what to do to achieve success"
+            plainText: ["I know exactly what to do to achieve success"]
         )
     ]
 }
